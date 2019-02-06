672527bc0b956a2b24a2113fc030306e658ebf0a
Steve Albonico <salbonico@gmail.com>
2019-02-06 23:19:43 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,6 +18,14 @@ end
 
 def save
 
+      sql = <<-SQL
+        INSERT INTO articles (title, content)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
 end
 
 end
