ca314477d5f17ae201d2e3407fe1a4d2260395a8
Steve Albonico <salbonico@gmail.com>
2019-02-06 23:51:04 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,16 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-@@all = []
+# @@all = []
 
 attr_accessor :title, :content
 attr_reader :id
 
-# def initiate(input)
-#   @title = input[:title]
-#   @content = input[:content]
-#   @@all << self
-# end
+def initiate(input)
+  @title = input[:title]
+  @content = input[:content]
+  @@all << self
+end
 
 #def self.all
 #@@all
