d96fe51b52c0a7a8c25064b6badbda10c68cca8d
Steve Albonico <salbonico@gmail.com>
2019-02-07 00:33:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
     article.content = params[:content]
     article.save
 
-    redirect '/articles/poo'
+    redirect '/articles/#{article.id}'
   end
 
 
