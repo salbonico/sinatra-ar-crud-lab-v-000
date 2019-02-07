0a8e3ed9121d4e455bfb8523d19352c69d297b78
Steve Albonico <salbonico@gmail.com>
2019-02-07 00:46:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,15 +32,16 @@ class ApplicationController < Sinatra::Base
     redirect "/articles/#{article.id}"
   end
 
-get 'articles/:id/edit' do
-  @article = Article.find(params[:id])
-  
-end
+
 
 get '/articles/:id' do
 @article = Article.find(params[:id])
 erb :show
 end
 
+get 'articles/:id/edit' do
+  @article = Article.find(params[:id])
+
+end
 
 end
