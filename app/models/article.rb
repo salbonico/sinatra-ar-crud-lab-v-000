e2def937fb69d29d4afa6216f7b7d9e5d17a8e10
Steve Albonico <salbonico@gmail.com>
2019-02-07 00:02:52 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,26 +6,5 @@ class Article < ActiveRecord::Base
 attr_accessor :title, :content
 attr_reader :id
 
-# def initiate(input)
-#   @title = input[:title]
-#   @content = input[:content]
-#
-# end
-
-#def self.all
-#@@all
-#end
-
-#def save
-
-#      sql = <<-SQL
-#        INSERT INTO articles (title, content)
-#        VALUES (?, ?)
-#      SQL
-
-#      ActiveRecord::Base[:database].execute(sql, self.title, self.content)
-
-
-#end
 
 end
