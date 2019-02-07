dce63174098b0d13a1ef5496143b689657ae7288
Steve Albonico <salbonico@gmail.com>
2019-02-07 01:04:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,8 +44,9 @@ erb :show
 end
 
 patch '/articles/:id' do
-  article = Article.find(params[:id])
   binding.pry
+  article = Article.find(params[:id])
+
   article.title = params[:title]
   article.content = params[:content]
   article.save
